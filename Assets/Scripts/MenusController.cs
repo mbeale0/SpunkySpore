3ee15c470547d9c5a705be90eabbd83ca9cc9a00
Mason Beale <mbeale@cedarville.edu>
2023-02-05 00:10:42 -0500

    Added buttons on level

--- Assets/Scripts/MenusController.cs
+++ Assets/Scripts/MenusController.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MenusController : MonoBehaviour
 {
     public void OnPlay()
     {
-        Debug.Log("Play");
+        SceneManager.LoadScene(1);
+    }
+    public void OnQuit()
+    {
+        Application.Quit();
     }
 }
